f867929e9a810cb66f8e3f509a68455ee8338c6b
Aloksh42 <alokbits02@gmail.com>
2023-10-20 23:14:23 +0530

    Auth & Authorization

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,14 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from "./routes/user";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
---
+++ server/src/routes/user.ts
@@ -0,0 +1,67 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { UserModel, IUser } from "../models/user";
+import { UserErrors } from "../errors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    newUser.save();
+    res.json({ message: "User Registered Succesfully" });
+  } catch (error) {
+    res.status(500).json({ type: error });
+  }
+});
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.body.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, process.env.SECRET, (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  }
+  return res.sendStatus(401);
+};
+
+router.post("/login", verifyToken, async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user: IUser = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET);
+    res.json({ token, userID: user._id });
+  } catch (error) {
+    res.status(500).json({ type: error });
+  }
+});
+
+export { router as userRouter };
