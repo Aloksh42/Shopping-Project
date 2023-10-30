837e6865b4111edcffae649b0da1ab59d72a591f
Aloksh42 <alokbits02@gmail.com>
2023-10-30 20:06:12 +0530

    context-file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,24 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ShopPage from "./pages/shop";
-import {AuthPage} from "./pages/auth";
+import { AuthPage } from "./pages/auth";
 import CheckoutPage from "./pages/checkout";
 import PurchasedItemsPage from "./pages/purchased-items";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ShopPage />}/>
-          <Route path="/auth" element={<AuthPage />}/>
-          <Route path="/checkout" element={<CheckoutPage />}/>
-          <Route path="purchased-items" element={<PurchasedItemsPage />}/>
-        </Routes>
+        <ShopContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ShopPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="purchased-items" element={<PurchasedItemsPage />} />
+          </Routes>
+        </ShopContextProvider>
       </BrowserRouter>
     </div>
   );
